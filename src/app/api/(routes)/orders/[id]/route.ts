cd6fa8501745af0d79f5a4aeeb752b866b40e3bb
0AliReza0 <alireza.abedi9310@gmail.com>
2023-10-12 21:08:26 +0330

    FEATURED: orders route handler added...

---
+++ src/app/api/(routes)/orders/[id]/route.ts
@@ -0,0 +1,26 @@
+import { adminAuth } from '@/app/api/auth/auth';
+import dbConnect from '@/app/api/utils/dbConnect';
+import { NextRequest, NextResponse as res } from 'next/server';
+
+export async function DELETE(req: NextRequest) {
+   await dbConnect();
+
+   const id = req.nextUrl.pathname.split('/')[3];
+
+   const data = await adminAuth(req, null);
+
+   if (data instanceof res) {
+      return data;
+   }
+
+   data.users[0].orders = data.users[0].orders?.filter(
+      (e) => e._id?.toString() !== id,
+   );
+
+   try {
+      await data.users.at(0)?.save();
+      return res.json({ status: 'done' });
+   } catch (err: any) {
+      return res.json({ message: err.message }, { status: 500 });
+   }
+}
